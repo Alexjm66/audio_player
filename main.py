579f8bb2e9330c234754fc5854de2e2a047d3df5
Alexjm66 <alexjmaart665@gmail.com>
2020-11-16 11:00:13 +0200

    Added Main

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import pygame
 from tkinter import filedialog
 root=Tk()
 root.title("")
-root.geometry("1000x350")
+root.config(bg="aqua")
+root.geometry("950x340")
 
 pygame.mixer.init()
 
@@ -15,10 +16,11 @@ def add_song():
 
 def play():
     song = lst_box.get(ACTIVE)
-    song = f"/home/user/Documents/python_exercises/audio_player/songs/{song}.wav"
+    song1 = f"/home/user/Documents/python_exercises/audio_player/songs/{song}.wav"
 
-    pygame.mixer.music.load(song)
+    pygame.mixer.music.load(song1)
     pygame.mixer.music.play(loops=0)
+    display['text']=song
 
 def pause():
     pygame.mixer.music.pause()
@@ -29,34 +31,38 @@ def unpause():
 def stop():
     pygame.mixer.music.stop()
 
-lblframe1=LabelFrame(root,text="Song Playing")
-lblframe1.pack(fill="both")
-lblframe1.place()
+def exit():
+    root.destroy()
 
-lblframe2=LabelFrame(root,text="Playlist")
-lblframe2.pack(fill="both")
-lblframe2.config(bg="aqua")
-lblframe2.place(x=450, y=10)
-lst_box=Listbox(lblframe2, bg="grey", fg="white", width=60)
+display = Label(root,text="", relief="groove", bg="grey", fg="white")
+display.pack()
+display.config(width=48, height=4)
+display.place(x = 5, y = 80)
+
+lblframe1=LabelFrame(root,text="Playlist")
+lblframe1.pack(fill="both")
+lblframe1.config(bg="red")
+lblframe1.place(x=410, y=10)
+lst_box=Listbox(lblframe1, bg="grey", fg="white", width=60)
 lst_box.pack(padx=20, pady=20)
 
+lblframe2=LabelFrame(root,text="Control Panel", padx=130, pady=10)
+lblframe2.pack(fill="both")
+lblframe2.config(bg="red")
+lblframe2.place(x = 10, y = 260)
 
-lblframe3=LabelFrame(root,text="Control Panel", padx=180, pady=10)
-lblframe3.pack(fill="both")
-lblframe3.config(bg="red")
-lblframe3.place(x = 80, y = 270)
-
-play_btn=Button(lblframe3, text="Play Song", command=play)
+play_btn=Button(lblframe2, text="Play Song", command=play)
 play_btn.grid(row=1, column=1, padx=10)
 
-pause_btn=Button(lblframe3, text="Pause Song", command=pause)
+pause_btn=Button(lblframe2, text="Pause Song", command=pause)
 pause_btn.grid(row=1, column=2,padx=10)
 
-unpause_btn=Button(lblframe3, text="Unpause", command=unpause)
+unpause_btn=Button(lblframe2, text="Unpause", command=unpause)
 unpause_btn.grid(row=1, column=3, padx=10)
 
-stop_btn=Button(lblframe3, text="Stop",width=8, command=stop)
+stop_btn=Button(lblframe2, text="Stop", command=stop)
 stop_btn.grid(row=1, column=4, padx=10)
+
 my_menu = Menu(root)
 root.config(menu=my_menu)
 
@@ -64,5 +70,8 @@ add_song_menu = Menu(my_menu)
 my_menu.add_cascade(label="Add Songs", menu=add_song_menu)
 add_song_menu.add_command(label="Add Song To Playlist", command=add_song)
 
+exit_btn=Button(root,text="Exit Program", width=24, height=2, command=exit)
+exit_btn.pack()
+exit_btn.place(x = 710, y = 275 )
 
 root.mainloop()
